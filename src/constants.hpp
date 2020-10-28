989d8a4857dc5e4ff0481cb1d5666d07d3fef42b
Alexander <megag96@mail.ru>
2020-10-28 03:21:44 +0300

    Обновлена версия до 0.10.0

--- src/constants.hpp
+++ src/constants.hpp
@@ -3,7 +3,7 @@
 
 namespace AxelChat {
 const QString APPLICATION_NAME    = "AxelChat";
-const QString APPLICATION_VERSION = "0.9.1";
+const QString APPLICATION_VERSION = "0.10.0";
 const QString ORGANIZATION_NAME   = "Axel_k";
 const QString ORGANIZATION_DOMAIN = "https://www.youtube.com/channel/UCujkj8ZgVkscm34GA1Z8wTQ";
 }
